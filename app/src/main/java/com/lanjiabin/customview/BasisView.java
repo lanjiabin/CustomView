0c51fd754ded9595e3a6fb28c887c512e75eb290
lanjiabin <1323457707@qq.com>
2020-02-25 11:26:09 +0800

    抗锯齿，仔细看有抗锯齿为true的，对面false的，有锯齿的，圆的周边有一个个小小的锯齿的样式。相反false则平滑，没有锯齿！

--- app/src/main/java/com/lanjiabin/customview/BasisView.java
+++ app/src/main/java/com/lanjiabin/customview/BasisView.java
@@ -43,11 +43,15 @@ public class BasisView extends View {
         super.onDraw(canvas);
 
         //设置画笔的基本属性
+        mPaint.setAntiAlias(false); //抗锯齿
         mPaint.setColor(getResources().getColor(R.color.orchid)); //设置画笔颜色
         mPaint.setStyle(Paint.Style.STROKE); //设置填充样式
         mPaint.setStrokeWidth(10); //设置画笔宽度
 
-        //画圆 cx x坐标 cy y坐标 radius 圆半径
+        //画圆 cx 圆心x坐标 cy 圆心y坐标 radius 圆半径
         canvas.drawCircle(190,200,150,mPaint);
+
+        mPaint.setAntiAlias(true); //抗锯齿
+        canvas.drawCircle(510,200,150,mPaint);
     }
 }

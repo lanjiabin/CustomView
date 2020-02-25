caff0f5b8c356545e2eebd44563042501680977b
lanjiabin <1323457707@qq.com>
2020-02-25 16:47:37 +0800

    1.画布的颜色设置 2.绘制一条线和多个线 3.绘制一个点和多个点

--- app/src/main/java/com/lanjiabin/customview/BasisView.java
+++ app/src/main/java/com/lanjiabin/customview/BasisView.java
@@ -2,6 +2,7 @@ package com.lanjiabin.customview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -20,7 +21,7 @@ public class BasisView extends View {
 
     /**
      * 默认的构造方法
-     * */
+     */
     public BasisView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         //初始化
@@ -33,9 +34,9 @@ public class BasisView extends View {
 
     /**
      * 初始化
-     * */
-    public void init(){
-        mPaint=new Paint();
+     */
+    public void init() {
+        mPaint = new Paint();
     }
 
     @Override
@@ -45,16 +46,69 @@ public class BasisView extends View {
         //设置画笔的基本属性
         mPaint.setAntiAlias(false); //抗锯齿
         mPaint.setColor(getResources().getColor(R.color.orchid)); //设置画笔颜色
-        mPaint.setStrokeWidth(10); //设置画笔宽度
+        mPaint.setStrokeWidth(30); //设置画笔宽度
 
         mPaint.setStyle(Paint.Style.STROKE); //设置填充样式 Paint.Style.STROKE 仅描边
-        canvas.drawCircle(190,200,150,mPaint);
+        canvas.drawCircle(190, 200, 150, mPaint);
 
         mPaint.setStyle(Paint.Style.FILL_AND_STROKE); //设置填充样式 Paint.Style.STROKE 填充内部和描边
-        canvas.drawCircle(510,200,150,mPaint);
+        canvas.drawCircle(510, 200, 150, mPaint);
 
         mPaint.setStyle(Paint.Style.FILL); //设置填充样式 Paint.Style.STROKE 仅填充内部，就是没有了圆边
-        canvas.drawCircle(830,200,150,mPaint);
+        canvas.drawCircle(830, 200, 150, mPaint);
+
+        /*
+          设置画布颜色
+          */
+        canvas.drawColor(Color.BLUE); //传入存在的颜色
+//        canvas.drawARGB(255,0,255,255); //传入ARGB颜色分量
+//        canvas.drawRGB(255,0,255); //传入RGB分量
+
+        /*
+          @startX 起始点X的坐标
+          @startY 起始点Y的坐标
+          @stopX 终点X的坐标
+          @stopY 终点Y的坐标
+          起点：（840,300） 终点：（840,600）
+         */
+        canvas.drawLine(840, 300, 840, 600, mPaint);
+
+        /*
+          绘制多条直线
+          数组中两两成为一个坐标
+          起点：（10，10） 终点：（100,100）
+          起点：（200,200）终点：（400,400）
+          */
+        float[] pts = {10, 10, 100, 100, 200, 200, 400, 400};
+        canvas.drawLines(pts, mPaint);
+
+        float[] past1 = {10, 100, 100, 200, 200, 300, 400, 500};
+        canvas.drawLines(past1, mPaint);
+
+        float[] past2 = {10, 200, 100, 300, 200, 400, 500, 600};
+        /*
+          @offset 集合中跳过的坐标个数，这里的坐标是指一个点（66,88）
+          @count 表示有多少个数值参与绘制，比如（66,88）（77,99）那就是四个数值
+          下面的offset=2，所以（10,200）（100，300）被跳过
+          下面的count=4，即（200,400）（500,600）这四个数值参与绘制
+          所以起点坐标是：（200,400） 终点坐标是：（500,600）
+          */
+        canvas.drawLines(past2, 2, 4, mPaint);
+
+        /*
+          线和点的大小与mPaint.setStrokeWidth(30);有关
+          与mPaint.setStyle无关
+          */
+        canvas.drawPoint(300,700,mPaint);
+
+        /*
+          与上面一致，多个点和跳过的点
+          */
+        float[] points = {300, 750, 250, 725, 260, 780, 210, 900};
+        canvas.drawPoints(points,mPaint);
+
+        float[] points2 = {300, 850, 250, 825, 260, 880, 210, 990};
+        canvas.drawPoints(points2,2,4,mPaint);
 
     }
 }
